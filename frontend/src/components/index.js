714fa65b8664eb6debe727baee0637d2def0ee26
Shahan-Waheed728 <shahanwaheed43@gmail.com>
2026-03-13 10:50:59 +0500

    Save backend work

--- frontend/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from './NavBar';
-export { default as Footer } from './Footer';
-export { default as Toast } from './Toast';
-export { default as Loader } from './Loader';
--- frontend/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './Home';
-export { default as Upload } from './Upload';
-export { default as Result } from './Result';
-export { default as About } from './About';
-export { default as Contact } from './Contact';
